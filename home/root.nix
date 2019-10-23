e1b4431004c959fb9e5603c027d1654dd30ee154
Serhiy Makarenko <serhiy.makarenko@me.com>
2019-10-23 18:38:13 +0300

    Add support of git to fish

--- home/root.nix
+++ home/root.nix
@@ -46,6 +46,18 @@
         shellAliases = {
           ll = "ls -lah $argv";
         };
+        shellInit = ''
+          set segment_separator \u258C
+          set -g current_bg 000
+          set -q color_git_dirty_bg; or set color_git_dirty_bg yellow
+          set -q color_git_dirty_str; or set color_git_dirty_str black
+          set -q color_git_bg; or set color_git_bg green
+          set -q color_git_str; or set color_git_str black
+          set -q fish_git_prompt_untracked_files; or set fish_git_prompt_untracked_files normal
+          set -g __fish_git_prompt_showdirtystate "yes"
+          set -g __fish_git_prompt_char_dirtystate "±"
+          set -g __fish_git_prompt_char_cleanstate ""
+        '';
         promptInit = ''
           function fish_greeting
             echo -----------------------------------------------------------------
@@ -68,9 +80,78 @@
             who -aH
             echo -----------------------------------------------------------------
           end
-          function fish_prompt
+          function prompt_segment -d "Function to draw a segment"
+            set -l bg
+            set -l fg
+            if [ -n "$argv[1]" ]
+              set bg $argv[1]
+            else
+              set bg normal
+            end
+            if [ -n "$argv[2]" ]
+              set fg $argv[2]
+            else
+              set fg normal
+            end
+            if [ "$current_bg" != '000' -a "$argv[1]" != "$current_bg" ]
+              set_color -b $bg
+              set_color $current_bg
+              echo -n "$segment_separator "
+              set_color -b $bg
+              set_color $fg
+            else
+              set_color -b $bg
+              set_color $fg
+              echo -n " "
+            end
+            set current_bg $argv[1]
+            if [ -n "$argv[3]" ]
+              echo -n -s $argv[3] " "
+            end
+          end
+          function prompt_git -d "Display the current git state"
+            set -l ref
+            set -l dirty
+            if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+              set dirty (parse_git_dirty)
+              set ref (command git symbolic-ref HEAD 2> /dev/null)
+              if [ $status -gt 0 ]
+                set -l branch (command git show-ref --head -s --abbrev | head -n1 2> /dev/null)
+                set ref "➦ $branch "
+              end
+              set branch_symbol \uE0A0
+              set -l branch (echo $ref | sed  "s-refs/heads/-$branch_symbol -")
+              if [ "$dirty" != "" ]
+                prompt_segment $color_git_dirty_bg $color_git_dirty_str "$branch $dirty"
+              else
+                prompt_segment $color_git_bg $color_git_str "$branch $dirty"
+              end
+            end
+          end
+          function parse_git_dirty
+            if [ $__fish_git_prompt_showdirtystate = "yes" ]
+              set -l submodule_syntax
+              set submodule_syntax "--ignore-submodules=dirty"
+              set untracked_syntax "--untracked-files=$fish_git_prompt_untracked_files"
+              set git_dirty (command git status --porcelain $submodule_syntax $untracked_syntax 2> /dev/null)
+              if [ -n "$git_dirty" ]
+                  echo -n "$__fish_git_prompt_char_dirtystate"
+              else
+                  echo -n "$__fish_git_prompt_char_cleanstate"
+              end
+            end
+          end
+          function prompt_finish -d "Close open segments"
+            if [ -n $current_bg ]
+              set_color normal
+              set_color $current_bg
+              echo -n "$segment_separator "
+              set_color normal
+            end
+            set -g current_bg 000
+          end
+          function user_prompt
             set -l color_cwd
-            set -l suffix
             set -l HOSTNAME (hostname)
             switch $USER
             case root toor
@@ -79,15 +160,33 @@
               else
                 set color_cwd $fish_color_cwd
               end
-                set suffix '#'
             case '*'
               set color_cwd $fish_color_cwd
+            end
+            echo -n -s [(set_color $color_cwd)(date "+%H:%M")(set_color normal)] ' ' "$USER" ' ' [(set_color $color_cwd) "$PWD" (set_color normal)] ' '
+          end
+          function user_suffix
+            set -l suffix
+            switch $USER
+            case root toor
+              set suffix '#'
+            case '*'
               set suffix '>'
             end
-            
-            echo -n -s [(set_color $color_cwd)(date "+%H:%M")(set_color normal)] ' ' "$USER" @ "$HOSTNAME" ' ' [(set_color $color_cwd) "$PWD" (set_color normal)] ' '"$suffix "
+            echo -n -s "$suffix "
+          end
+          function fish_prompt
+            user_prompt
+            if [ -d .git ]
+              type -q git; and prompt_git
+              prompt_finish
+            end
+            user_suffix
           end
         '';
+      };
+      command-not-found = {
+        enable = true;
       };
   	};
   };
